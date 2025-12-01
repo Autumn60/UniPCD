c0bb0738bacdd830ba344e1e2285a1be22dd6d48
Autumn60 <harada.akiro@gmail.com>
2025-12-01 14:01:19 +0900

    multiple import
    Signed-off-by: Autumn60 <harada.akiro@gmail.com>

--- Assets/UniPCD/Editor/PCDMultipleImporterEditorWindow.cs
+++ Assets/UniPCD/Editor/PCDMultipleImporterEditorWindow.cs
@@ -7,6 +7,9 @@ namespace UniPCD.Editor
 {
   public class PCDMultipleImporterEditorWindow : EditorWindow
   {
+    private string _fileName = "MergedPCD";
+    private int _skipRate = 1;
+
     [MenuItem("UniPCD/Import .pcd directory")]
     public static void ShowWindow()
     {
@@ -17,28 +20,22 @@ namespace UniPCD.Editor
     {
       GUILayout.Label("Import .pcd directory", EditorStyles.boldLabel);
 
+      _fileName = EditorGUILayout.TextField("File Name", _fileName);
+      _skipRate = EditorGUILayout.IntSlider("Skip Rate", _skipRate, 1, 1000);
+
       if (GUILayout.Button("Import"))
       {
         string path = EditorUtility.OpenFolderPanel("Import .pcd directory", "", "");
         if (path.Length != 0)
         {
           string[] files = Directory.GetFiles(path, "*.pcd");
-          List<string> importedFiles = new List<string>();
-          foreach (string file in files)
+          if (PCDImporter.Import(files, _fileName, _skipRate))
           {
-            if (PCDImporter.Import(file))
-            {
-              string fileName = Path.GetFileNameWithoutExtension(file);
-              importedFiles.Add(fileName);
-            }
-            else
-            {
-              Debug.LogError($"Failed to read {file}");
-            }
+            Debug.Log($"Imported {_fileName}.asset (skip rate: {_skipRate})");
           }
-          if (importedFiles.Count > 0)
+          else
           {
-            Debug.Log($"Imported {string.Join(", ", importedFiles.ToArray())}");
+            Debug.LogError("Failed to read .pcd files");
           }
         }
       }
